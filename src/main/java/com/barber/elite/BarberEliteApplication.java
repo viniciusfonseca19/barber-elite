24d0289f503a2ed56a552cc05feac3e663126507
Vinícius F. <fonsecavinicius117@gmail.com>
2026-04-08 21:30:03 -0300

    feat: initialization of the backend application and database

--- src/main/java/com/barber/elite/BarberEliteApplication.java
+++ src/main/java/com/barber/elite/BarberEliteApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BarberEliteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BarberEliteApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BarberEliteApplication.class, args);
+    }
 }
